a0c05414a97649c4bb956a8e8760c8a27bdc593f
matthewsidneyjacobs <matthewsidneyjacobs@gmail.com>
2016-11-16 16:00:44 -0700

    working on extra things now

--- js/mainCtrl.js
+++ js/mainCtrl.js
@@ -5,7 +5,9 @@ angular.module('chatroom').controller('mainCtrl', function($scope, messageServic
 
     $scope.getMessages = function() {
       messageService.getMessages().then(function(response){
-        $scope.messages = response.data;
+        $scope.messages = response.data
+
+        // console.log(response)
       })
     }
 
